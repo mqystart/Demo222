cbc75b367bee702d34b96aaa8285a21ee85665e1
hang <hang@itcast.cn>
2019-07-13 21:47:45 +0800

    Initial commit

---
+++ springbootdemo22/src/main/java/com/leyou/controller/Hello.java
@@ -0,0 +1,28 @@
+package com.leyou.controller;
+
+/**
+ * @author Andy
+ * @create 2019-07-13 16:18
+ */
+public class Hello {
+    public static void main(String[] args) {
+
+
+
+        System.out.println();
+
+
+
+    }
+
+            
+    public static void Create(String method, String month, String date) {
+
+        
+    }
+
+    public void aaa() {
+
+
+    }
+}
